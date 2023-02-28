cb9c6d52d8ee65128775b2422ec1404f957c00e4
efforthye <efforthye@gmail.com>
2023-02-28 22:07:52 +0900

    임시저장

--- Maple/mapleweb/src/components/Administrator/Account/Container.jsx
+++ Maple/mapleweb/src/components/Administrator/Account/Container.jsx
@@ -2,6 +2,7 @@ import AccountComponent from "./Component";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+// 관리자 목록 요청 함수
 const tempListFun = async (setList) => {
   try {
     let listArr = (await axios.post("/api/admin/list"))
@@ -13,10 +14,14 @@ const tempListFun = async (setList) => {
 };
 
 const AccountContainer = () => {
+
+  // 관리자 목록
   const [listArr, setList] = useState([]);
+
+  // 관리자 등록, DB 연동
   const onSubmit = async (value) => {
-    if(!value.id.match(/\S/g) || !value.password.match(/\S/g) ||!value.adminName.match(/\S/g))
-    return alert("공간채워라잉");
+    if (!value.id.match(/\S/g) || !value.password.match(/\S/g) || !value.adminName.match(/\S/g))
+      return alert("공간채워라잉");
     axios
       .post("/api/admin/regist", value)
       .then(({ data }) => {
@@ -25,10 +30,12 @@ const AccountContainer = () => {
       });
   };
 
+  // 관리자 목록 출력
   useEffect(() => {
     tempListFun(setList);
   }, []);
 
+  // 관리자 목록 삭제
   const onClick = async (idx) => {
     axios.post("/api/admin/delete", { idx }).then(() => {
       alert("삭제됐습니다.");
@@ -36,6 +43,7 @@ const AccountContainer = () => {
     });
   };
 
+  // 컴포넌트
   return (
     <AccountComponent onSubmit={onSubmit} listArr={listArr} onClick={onClick} />
   );
